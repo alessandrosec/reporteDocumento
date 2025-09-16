2a1a393e87427c0e19102e3cb06d4145d1665336
alessandrosec <secgeovanny@gmail.com>
2025-09-16 01:03:57 -0600

    actualización de código

---
+++ universidad_backend/app/routes/validacionCursos.routes.js
@@ -0,0 +1,8 @@
+module.exports = app => {
+    const validacionCursos = require ("../controllers/validacionCursos.controller.js");
+    var router = require("express").Router();
+
+    router.get("/validacion-cursos/:id");
+    router.get("/cursos-aprobados/:id", soloEstudiantesYAdmin.getCursosAprobados);
+    router.get("/validacion-cursos/id/pdf")
+}
